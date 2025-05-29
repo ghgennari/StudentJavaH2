3aac18ef6e0af53cb0d8edd400b1a34b44c654f1
Gustavo <ghgennari@gmail.com>
2025-05-29 10:15:37 -0300

    Mudança de BD
    Implementação do BD java H2 no projeto angular.

--- src/main/java/br/gov/sp/fatec/itu/student/controllers/StudentController.java
+++ src/main/java/br/gov/sp/fatec/itu/student/controllers/StudentController.java
@@ -10,12 +10,17 @@ import br.gov.sp.fatec.itu.student.repositories.StudentRepository;
 import br.gov.sp.fatec.itu.student.services.StudentService;
 
 import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("students")
 public class StudentController {
@@ -32,5 +37,18 @@ public class StudentController {
     public ResponseEntity<Student> save(@RequestBody Student student){
         return ResponseEntity.created(null).body(service.save(student));
     }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void>delete(@PathVariable long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> update(@PathVariable long id, @RequestBody Student student){
+        service.update(student, id);
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
--- src/main/java/br/gov/sp/fatec/itu/student/services/StudentService.java
+++ src/main/java/br/gov/sp/fatec/itu/student/services/StudentService.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import br.gov.sp.fatec.itu.student.entities.Student;
 import br.gov.sp.fatec.itu.student.repositories.StudentRepository;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class StudentService {
@@ -20,5 +22,23 @@ public class StudentService {
 
     public Student save(Student student){
         return repository.save(student);
+    }
+
+    public void update(Student student, Long id){
+        Student aux = repository.getReferenceById(id);
+
+        aux.setCourse(student.getCourse());
+        aux.setName(student.getName());
+
+        repository.save(aux);
+    }
+
+    public void delete(@PathVariable long id){
+        if(repository.existsById(id)){
+            repository.deleteById(id);
+        }
+        else{
+            throw new EntityNotFoundException("Aluno não cadastrado");
+        }
     }
 }
